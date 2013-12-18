6085b7f4438aac432b08772ce21671e1e3317abc
me <me@gnu-linux>
2013-12-18 05:41:30 -0500

    refactoring

--- src/universal.d/xr_uint_octet_HANDLE.cpp
+++
@@ -1,3 +0,0 @@
-  //xr(...), "Reverse Receive": connects to IPv4Address on tcpPort and writes any data recieved to the file attached to the handle dataIn. The argument IPv4Address is assumed to be exactly four bytes long, with each byte representing a single octet. 
-
-return NET_FAILURE_NOS;
--- src/universal.d/xr_uint_octet_MemBoundry.cpp
+++
@@ -1,3 +0,0 @@
-//xr(...), "Reverse Receive": connects to IPv4Address on tcpPort and writes any data recieved to dataOut.highest_block. The argument IPv4Address is assumed to be exactly four bytes long, with each byte representing a single octet. 
-
-return NET_FAILURE_NOS;
